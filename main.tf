aa3b102c50247a6caa673626e233cfaabe97afa0
Jordan Perry <jordanperry@manta.home>
2026-05-26 20:47:21 -0700

    Added Keycloak CT

--- main.tf
+++ main.tf
@@ -265,12 +265,12 @@ resource "proxmox_virtual_environment_container" "blog" {
   }
 
   cpu {
-    cores = 1
+    cores = 2
   }
 
   memory {
-    dedicated = 512
-    swap      = 512
+    dedicated = 2048
+    swap      = 2048
   }
 
   operating_system {
@@ -289,4 +289,55 @@ resource "proxmox_virtual_environment_container" "blog" {
       network_interface[0].mac_address,
     ]
   }
+}
+resource "proxmox_virtual_environment_container" "keycloak" {
+  node_name    = "leviathan"
+  vm_id        = 104
+  description  = "Keycloak IAM"
+
+  initialization {
+    hostname = "keycloak"
+
+    ip_config {
+      ipv4 {
+        address = "192.168.40.30/24"
+        gateway = "192.168.40.1"
+      }
+    }
+
+    dns {
+      servers = ["192.168.40.1"]
+    }
+  }
+
+  network_interface {
+    name     = "eth0"
+    bridge   = "vmbr0"
+    vlan_id  = 40
+    firewall = false
+  }
+
+  disk {
+    datastore_id = "wreck-lvm"
+    size         = 20
+  }
+
+  cpu {
+    cores = 2
+  }
+
+  memory {
+    dedicated = 1024
+  }
+
+  operating_system {
+    template_file_id = "local:vztmpl/debian-12-standard_12.12-1_amd64.tar.zst"
+    type             = "debian"
+  }
+  
+  features {
+  nesting = true
+}
+
+  start_on_boot = true
 }
